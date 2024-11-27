2e3cca8a02fbb2fc25a1faa138d7368a68206de0
sharifu majid <sharifumajid3@gmail.com>
2024-11-27 13:28:13 +0300

    added suspense

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,68 +1,9 @@
 "use client";
 
-import Form from "@components/Form";
 import UpdatePromptContent from "@components/UpdatePromptContent";
-import { useSession } from "next-auth/react";
-import { useRouter, useSearchParams } from "next/navigation";
-import React, { Suspense, useEffect, useState } from "react";
+import React, { Suspense } from "react";
 
 const UpdatePrompt = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams();
-  const promptId = searchParams.get("id");
-
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt: "",
-    tag: "",
-  });
-
-  const updateDataPrompt = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-
-    if (!promptId) return alert("Prompt Id not provided");
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`, {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
-      } else {
-        const error = await response.text();
-        console.error("Error updating prompt:", error);
-      }
-    } catch (error) {
-      console.error("Failed to update prompt:", error);
-    } finally {
-      setSubmitting(false);
-    }
-  };
-
-  useEffect(() => {
-    const getPromptDetails = async () => {
-      const response = await fetch(`api/prompt/${promptId}`);
-
-      const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      });
-    };
-
-    if (promptId) getPromptDetails();
-  }, [promptId]);
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <UpdatePromptContent />
